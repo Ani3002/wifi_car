1817c087b15fbf45d792c0507d0e754e059d783b
Anirban Routh <97686698+Ani3002@users.noreply.github.com>
2023-07-31 17:52:08 +0530

    Update wifi_car.ino

--- wifi_car.ino
+++ wifi_car.ino
@@ -1,9 +1,9 @@
-#define ENA   14          // Enable/speed motors Right        GPIO14(D5)
-#define ENB   12          // Enable/speed motors Left         GPIO12(D6)
-#define IN_1  15          // L298N in1 motors Rightx          GPIO15(D8)
-#define IN_2  13          // L298N in2 motors Right           GPIO13(D7)
-#define IN_3  2           // L298N in3 motors Left            GPIO2(D4)
-#define IN_4  0           // L298N in4 motors Left            GPIO0(D3)
+#define ENA   14                  GPIO14(D5)
+#define ENB   12                  GPIO12(D6)
+#define IN_1  15                  GPIO15(D8)
+#define IN_2  13                  GPIO13(D7)
+#define IN_3  2                   GPIO2(D4)
+#define IN_4  0                   GPIO0(D3)
 #define MD    10           // Metal detector
 #define ledPin 5
 #define ledPin1 4
